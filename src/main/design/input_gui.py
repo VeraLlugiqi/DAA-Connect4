5e2a8744580a247c078589da5741682ee66f758b
AnjezaSfishta <anjeza.sfishta@student.uni-pr.edu>
2024-01-06 23:00:44 +0100

    Update input_gui.py

--- src/main/design/input_gui.py
+++ src/main/design/input_gui.py
@@ -8,6 +8,7 @@ class ConnectFourSetup:
     def __init__(self):
         self.player_name = None
         self.grid_size = None
+        self.game_mode = None
 
         self.window = tk.Tk()
         self.window.title("Connect Four Setup")
@@ -22,9 +23,13 @@ class ConnectFourSetup:
         padding_label = tk.Label(self.window, text="", bg="white")
         padding_label.pack(pady=15)
 
-        tk.Label(self.window, text="Write your name:", bg="white",font=("Helvetica", 15)).pack(pady=10)
-        self.name_entry = customtkinter.CTkEntry(self.window, placeholder_text="Your Name")
-        self.name_entry.pack(pady=5) 
+        input_frame = tk.Frame(self.window, bg="white")
+        input_frame.pack(pady=10)
+
+
+        tk.Label(input_frame, text="Write your name:", bg="white",font=("Helvetica", 15)).grid(row=0, column=0, pady=5, padx=5)
+        self.name_entry = customtkinter.CTkEntry(input_frame, placeholder_text="Your Name")
+        self.name_entry.grid(row=0, column=1, pady=5, padx=5)
 
         tk.Label(self.window, text="Choose Table Size (rows x columns)",bg="white",font=("Helvetica", 15)).pack(pady=10)
         self.grid_size_var = customtkinter.StringVar()
@@ -33,27 +38,28 @@ class ConnectFourSetup:
         size_dropdown.set("6x7")  # Default value
         size_dropdown.pack(pady=5)
 
-        tk.Label(self.window, text="Game Mode:",bg="white",font=("Helvetica", 15)).pack(pady=5)
-        self.grid_size_var = customtkinter.StringVar()
-        size_options = ["User vs User", "User vs Comp"]
-        size_dropdown = customtkinter.CTkComboBox(self.window, variable=self.grid_size_var, values=size_options)
-        size_dropdown.set("User vs Comp")  # Default value
-        size_dropdown.pack(pady=5)
+        button_frame = tk.Frame(self.window, bg="white")
+        button_frame.pack(pady=5)
+
+        user_vs_user_button = tk.Button(button_frame, text="🧑 vs 🧑", font=("Helvetica", 22), command=lambda: self.set_game_mode("User vs User"), width=15,height=3,bg="yellow")
+        user_vs_user_button.pack(side="left", padx=10)
+
+        user_vs_comp_button = tk.Button(button_frame, text="🧑 vs 💻", font=("Helvetica", 22), command=lambda: self.set_game_mode("User vs Comp"), width=15,height=3,bg="yellow")
+        user_vs_comp_button.pack(side="left", padx=10)
 
         
-        button = customtkinter.CTkButton(master=self.window, corner_radius=10,text="Start", fg_color=("red", "lightgray"), command=self.submit_callback)
-        button.pack(pady=20)
+        button = tk.Button(master=self.window,text="Start", font=("Helvetica", 12),command=self.submit_callback, width=10, height=2,bg="red")
+        button.pack(pady=40)
 
-
+    def set_game_mode(self, mode):
+        self.game_mode = mode
     def get_player_name(self):
         return self.name_entry.get()
 
     def get_grid_size(self):
         return self.grid_size_var.get()
 
-    # def get_game_mode(self):
-    #     return mode_var.get()
-
+    
     def submit_callback(self):
         self.player_name = self.get_player_name()
         self.grid_size = self.get_grid_size()
