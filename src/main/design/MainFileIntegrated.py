2de6491f9c278f1e91fa4f36bd577c51b9d4ef38
Rinesa Zuzaku <119458053+RinesaZuzaku@users.noreply.github.com>
2024-01-13 16:02:17 +0100

    added refresh button

--- src/main/design/MainFileIntegrated.py
+++ src/main/design/MainFileIntegrated.py
@@ -39,8 +39,8 @@ class ConnectFourGUI:
         tk.Label(button_frame, text='', bg='white').pack(side='left', padx=50)
 
         button_width = 5
-       # self.refresh_button = tk.Button(button_frame, text='🔄', command=self.refresh, font=('Helvetica', 12), width=button_width, bg='yellow', fg='black')
-        #self.refresh_button.pack(side='left', padx=20)
+        self.refresh_button = tk.Button(button_frame, text='🔄', command=self.refresh, font=('Helvetica', 12), width=button_width, bg='yellow', fg='black')
+        self.refresh_button.pack(side='left', padx=20)
 
         self.close_button = tk.Button(button_frame, text='❌', command=self.close_window, font=('Helvetica', 12), width=button_width, bg='yellow', fg='black')
         self.close_button.pack(side='right', padx=20)
@@ -137,8 +137,11 @@ class ConnectFourGUI:
       
   # Add any additional logic you want when the game is over
 
-def refresh(self):
-    pass
+    def refresh(self):
+        # Reset the game state
+        self.board = create_board()
+        self.draw_board()
+        self.connect_four_label.config(text='Connect Four', fg='yellow')
 
     def count_down(self, total_seconds):
         while total_seconds >= 0:
